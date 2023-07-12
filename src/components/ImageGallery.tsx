6cd81e7520d3993d3af635f2278044b71e6d4379
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-07-12 08:32:59 -0700

    chore: add prettier class sorting for tailwind

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -6,7 +6,7 @@ export default function ImageGallery({ imagesData }: any) {
          {imagesData.map((img: any) => (
             <div
                key={img.id}
-               className="w-full h-full bg-center rounded-md bg-cover"
+               className="flex h-full w-full rounded-md bg-cover bg-center"
                style={{ backgroundImage: `url(${img.url})` }}
             ></div>
             // <img key={img.id} src={img.url} />
