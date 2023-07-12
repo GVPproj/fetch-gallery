77249ede4c89d2faba5c6c0595c347bc8fed9567
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-07-12 07:34:05 -0700

    feat: fetch and log some JSONPlaceholder data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import SiteHeader from "./components/SiteHeader";
 import ImageGallery from "./components/ImageGallery";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let response = await fetch(
+      "https://jsonplaceholder.typicode.com/albums/1/photos"
+    );
+    let data = await response.json();
+    const slicedArray = data.slice(0, 8);
+    console.log(slicedArray);
+  };
   return (
     <>
       <SiteHeader />
