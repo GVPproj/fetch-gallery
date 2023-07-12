6321238e84669d2906b9e885e46abc5a627a8c32
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-07-12 09:10:49 -0700

    fix: disable state temporarily to enable build

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 
 export default function ImageGallery({ imagesData }: any) {
-   const [index, setIndex] = useState(0)
+   //    const [index, setIndex] = useState(0)
 
    return (
       <>
          <div
-            className="relative flex h-full w-full items-center justify-center rounded-md border-4 border-neutral-100 bg-cover bg-center shadow-md shadow-neutral-300"
-            style={{ backgroundImage: `url(${imagesData[index].url})` }}
+            className="relative flex h-full w-full items-center justify-center 
+            rounded-lg border-4 border-neutral-100 bg-cover bg-center 
+            shadow-md shadow-neutral-300"
+            style={{ backgroundImage: `url(${imagesData[1].url})` }}
          >
             <div className="absolute max-w-[85%] rotate-30 text-center">
-               {imagesData[index].title}
+               {imagesData[1].title}
             </div>
          </div>
       </>
