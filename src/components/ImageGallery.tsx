9366b2b71a0d8b30f03abeff6a7ede0bd86ae9c1
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-07-12 08:09:57 -0700

    fix: temp any type

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,8 +1,8 @@
-export default function ImageGallery({ imagesData }) {
+export default function ImageGallery({ imagesData }: any) {
   //   console.log(imagesData);
   return (
     <>
-      {imagesData.map((img) => (
+      {imagesData.map((img: any) => (
         <img key={img.id} src={img.url} />
       ))}
     </>
