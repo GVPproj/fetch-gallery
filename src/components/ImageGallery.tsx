8060ea6122af5130f2efb593788d6d05679d645c
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-07-12 10:59:29 -0700

    feat: incrementing and decrementing through images, rendering at the index

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,18 +1,48 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 export default function ImageGallery({ imagesData }: any) {
-   //    const [index, setIndex] = useState(0)
+   const [index, setIndex] = useState(0)
+
+   function decrement() {
+      if (index === 0) {
+         setIndex(imagesData.length - 1)
+         console.log(index)
+      } else {
+         setIndex((prev) => prev - 1)
+      }
+   }
+   function increment() {
+      if (index === imagesData.length - 1) {
+         setIndex(0)
+         console.log(index)
+      } else {
+         setIndex((prev) => prev + 1)
+      }
+   }
 
    return (
       <>
+         <p>{index}</p>
          <div
-            className="relative flex h-full w-full items-center justify-center 
+            className="relative flex h-full w-full items-center justify-center gap-8
             rounded-lg border-4 border-neutral-100 bg-cover bg-center 
             shadow-md shadow-neutral-300"
-            style={{ backgroundImage: `url(${imagesData[1].url})` }}
+            style={{ backgroundImage: `url(${imagesData[index].url})` }}
          >
+            <button
+               onClick={decrement}
+               className=" absolute left-4 bg-neutral-100 bg-opacity-30"
+            >
+               &#60;
+            </button>
+            <button
+               onClick={increment}
+               className=" absolute right-4 bg-neutral-100 bg-opacity-30"
+            >
+               &#62;
+            </button>
             <div className=" max-w-[85%] rotate-28 text-center sm:rotate-30">
-               {imagesData[1].title}
+               {imagesData[index].title}
             </div>
          </div>
       </>
