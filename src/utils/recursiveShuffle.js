36f966c384c79a2872573bd47e41cfb7f9c8911a
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-07-13 06:15:13 -0700

    docs: recursive function comments

--- src/utils/recursiveShuffle.js
+++ src/utils/recursiveShuffle.js
@@ -3,10 +3,10 @@
 export default function recursiveShuffle(arr) {
     let shuffled = [] // array to return each time
     if (!arr.length) {
-       return shuffled // BASE CASE: break out of recursion when source array has no elements
+       return shuffled // BASE CASE: break out of recursion if source array has no elements
     }
     let i = Math.floor(Math.random() * arr.length) // a random index from within array length
     shuffled.push(arr[i]) // push randomly indexed element from old array into new array
     let slicedArray = arr.slice(0, i).concat(arr.slice(i + 1)) // a new array w/o pushed element
-    return shuffled.concat(recursiveShuffle(slicedArray)) // RECURSION: return shuffled array merged w/ self-call(on sliced array)
+    return shuffled.concat(recursiveShuffle(slicedArray)) // RECURSIVE CASE: return shuffled array merged w/ self-call(on sliced array)
  }
